1279855f0057d998e5a5a8fd0169e03f07c29cb1
YG <86285880+Yashgadwal@users.noreply.github.com>
2022-05-23 20:31:58 +0530

    Add files via upload

---
+++ timeLogErrorWarnTableAssertClear.js
@@ -0,0 +1,7 @@
+console.time('Your Code Took');
+console.log("This is how we Write in Console");
+console.error("This is how we Write Error in Console");
+console.warn("This is how we Write WArning in Console");
+console.table({name:"yash",Skill:"Coding,Hacking"});
+// console.clear();
+console.assert(100>189,'This is not Possible');
