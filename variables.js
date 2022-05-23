b9a987a2db2ea7d5302a63ad183a09a586b89566
YG <86285880+Yashgadwal@users.noreply.github.com>
2022-05-23 20:36:53 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,10 @@
+// Variables in JS
+/*
+We can create variables from using
+var , let , const
+*/
+
+//Please not use var because
+//  it is use before modern javascript 
+// after modern javascript
+//  we use let and const
