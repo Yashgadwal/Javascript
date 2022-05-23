340a582596c11e610c760dbd6b23d5723111c638
YG <86285880+Yashgadwal@users.noreply.github.com>
2022-05-23 21:57:53 +0530

    Add files via upload

---
+++ StringMethods,length,tolowercase,touppercase,indexof,lastidexof,charat,endswith,includes,substring,slice,slice,split,replace,innerHTML.js
@@ -0,0 +1,34 @@
+const name = "Strin";
+const Greeting = "Hello";
+let forStr ="Lofrem ipsum dolor sit amet consectetur adipisicing elit. Laborum maiores, nostrum cupiditate accusantium minima nisi voluptate atque nulla molestiae repudiandae? Ducimus doloremque saepe eius sed aut, porro debitis dignissimos minima, amet deserunt labore consequuntur reprehenderit aspernatur velit nisi illo libero?"
+
+console.log(name.concat('g'));
+
+// String Methods 
+
+// console.log(forStr.length);
+// console.log(forStr.toLowerCase());
+// console.log(forStr.toUpperCase());
+// console.log(forStr[4]);
+// console.log(forStr.indexOf('ipsum')); 
+// console.log(forStr.lastIndexOf('Lorem')); 
+// console.log(forStr.charAt(2));
+// console.log(forStr.endsWith('?'));
+// console.log(forStr.includes('f'));
+// console.log(forStr.substring(0,6));
+// console.log(forStr.slice(-1));
+// console.log(forStr.split(' '));
+// console.log(forStr.replace('Lofrem','Peter Parker'));
+
+let fruit1 = 'orange\'s';
+let fruit2 = 'apple';
+let name1 = "Raghav";
+
+let htmll = `Hello<b> ${name1}</b>
+<h1>this is heading </h1>
+<p>Your like ${fruit1} or ${fruit2}</p>
+`;
+document.body.innerHTML  = htmll;
+
+
+
