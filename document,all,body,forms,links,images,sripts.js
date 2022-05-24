d3fdb4a0d8c8b234f510c1f651243fa7d41e0b4e
YG <86285880+Yashgadwal@users.noreply.github.com>
2022-05-24 17:37:26 +0530

    Add files via upload

---
+++ document,all,body,forms,links,images,sripts.js
@@ -0,0 +1,25 @@
+Video URL = https://youtu.be/vzuOQnfOk2I?list=PLu0W_9lII9ajyk081To1Cbt2eI5913SsL
+
+let a = document;
+
+console.log(
+    document.all //Show All Tags
+);
+
+console.log(
+    document.body
+);
+
+console.log(
+    document.forms
+);
+
+let a = document.all;
+
+// Array.from(a).forEach(function(element){
+    document.write(element);
+})
+
+console.log(
+    document.links[0]
+);
