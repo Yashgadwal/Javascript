fb26ea12dee39bd4e71e1816b99b099c493f0b82
YG <86285880+Yashgadwal@users.noreply.github.com>
2022-05-24 16:57:52 +0530

    Add files via upload

---
+++ IfElse&SwichCase.js
@@ -0,0 +1,33 @@
+// && and , || or 
+const age = 16;
+
+if (age >= 18) {
+    console.log("you are able to use Phone");
+}
+
+else if (age !== 16) {
+    console.log("Go and play games");
+}
+
+else {
+    console.log("you are NOT able to use Phone");
+}
+
+console.clear();
+
+let name = "Code With Harry";
+
+switch (name) {
+    case "Code With Harry":
+        console.log("Best Channel for Coding is " + name);
+        break;
+
+    case "Raistar":
+        console.log("Best Channel for Free fire OneTap is " + name);
+        break;
+
+    default:
+        console.log("Nothing");
+        break;
+}
+
